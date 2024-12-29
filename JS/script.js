f7eecd2053385b85cb035504505bfbe1de6d03f4
Patrycja Holda <patrycjafirma@gmail.com>
2024-12-29 23:47:40 +0100

    added arrow function name showHideImage & changed "let" to "const"

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,24 @@
-function welcome() {
-    console.log("Cześć! Miłego dnia. Niech się darzy");
-}
-
-welcome();
-
-let button = document.querySelector(".button");
-let image = document.querySelector(".image");
-let section = document.querySelector("#strona-glowna");
-
-button.addEventListener("click", () => {
-    if (button.innerText === "Pokaż obrazek") {
-        section.appendChild(image);
-        button.innerText = "Usuń obrazek";
-    } else {
-        image.remove();
-        button.innerText = "Pokaż obrazek";
+{
+    const welcome = () => {
+        console.log("Cześć! Miłego dnia. Niech się darzy");
     }
-});
+    
+    welcome();
+    
+    const button = document.querySelector(".button");
+    const image = document.querySelector(".image");
+    const section = document.querySelector("#strona-glowna");
+    
+   const showHideImage = button.addEventListener("click", () => {
+        if (button.innerText === "Pokaż obrazek") {
+            section.appendChild(image);
+            button.innerText = "Usuń obrazek";
+        } else {
+            image.remove();
+            button.innerText = "Pokaż obrazek";
+        }
+    });
+
+init ();
+
+}
