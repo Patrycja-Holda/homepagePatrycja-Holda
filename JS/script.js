eff414cf52b587f83b0afeea88af8fd65afcf9f6
Patrycja Holda <patrycjafirma@gmail.com>
2025-01-02 20:07:37 +0100

    update JS - split code to function; (update const showHideImage)

--- JS/script.js
+++ JS/script.js
@@ -2,21 +2,26 @@
     const welcome = () => {
         console.log("Cześć! Miłego dnia. Niech się darzy");
     }
-    
-    welcome();
-    
-    const button = document.querySelector(".button");
-    const image = document.querySelector(".image");
-    const section = document.querySelector("#strona-glowna");
-    
-   const showHideImage = button.addEventListener("click", () => {
-        if (button.innerText === "Pokaż obrazek") {
-            section.appendChild(image);
-            button.innerText = "Usuń obrazek";
-        } else {
-            image.remove();
-            button.innerText = "Pokaż obrazek";
-        }
-    });
+
+    const showHideImage = () => {
+        const button = document.querySelector(".button");
+        const image = document.querySelector(".image");
+        const section = document.querySelector("#strona-glowna");
+
+        welcome();
+
+        button.addEventListener("click", () => {
+            if (button.innerText === "Pokaż obrazek") {
+                section.appendChild(image);
+                button.innerText = "Usuń obrazek";
+            } else {
+                image.remove();
+                button.innerText = "Pokaż obrazek";
+            }
+        });
+
+    }
+
+    showHideImage();
 
 }
